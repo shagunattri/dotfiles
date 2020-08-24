3ff49c563b9276eff323758fceb3cb8c194e9f1f
shagunattri <shagunattri17@gmail.com>
2020-08-24 17:37:27 +0530

    pfetch.sh

---
+++ pfetch.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+wget https://github.com/dylanaraps/pfetch/archive/master.zip
+unzip master.zip
+sudo install pfetch-master/pfetch /usr/local/bin/
+ls -l /usr/local/bin/pfetch
+echo "pfetch Installed!..."
